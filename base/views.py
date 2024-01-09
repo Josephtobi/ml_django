ace30887723d62a96442d71d6510bf6d55260ba2
Josephtobi <josephtobi53@gmail.com>
2024-01-09 15:45:08 +0100

    Update views.py

--- base/views.py
+++ base/views.py
@@ -3,8 +3,5 @@ from django.shortcuts import render
 # Create your views here.
 
 
-from django.shortcuts import render
-
-
 def index(request):
-    return render(request, 'render/index.html', {})
+    return render(request, 'index.html', {})
